f9457dbf00e1ba223110b5a39aeaba4cd234f652
Marcio <marcio.grazianni@gmail.com>
2022-01-22 12:01:15 -0300

    Melhorias no layout.

--- imobi/settings.py
+++ imobi/settings.py
@@ -106,8 +106,14 @@ TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
+# Parâmetros para valores
+DECIMAL_SEPARATOR = ','
+THOUSAND_SEPARATOR = '.'
+USE_THOUSAND_SEPARATOR = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- plataforma/admin.py
+++ plataforma/admin.py
@@ -5,7 +5,7 @@ from .models import DiasVisita, Horario, Imovei, Cidade, Imagem, Visitas
 @admin.register(Imovei)
 class ImoveiAdmin(admin.ModelAdmin):
     list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
-    list_editable = ('valor', 'tipo')
+    list_editable = ('valor', 'tamanho', 'cidade', 'tipo')
     list_filter = ('cidade', 'tipo')
     
 admin.site.register(DiasVisita)
