72f57c941902c74b58428c1d609ccc06278de552
marcio <marcio.grazianni@gmail.com>
2022-01-27 06:45:44 -0300

    Reorganização do settings.

--- imobi/settings.py
+++ imobi/settings.py
@@ -130,9 +130,9 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join('static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
